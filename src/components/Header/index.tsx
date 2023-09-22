8122b0ff0726daf107d936f868d7f55b053a3792
Tiago <trsminfo@gmail.com>
2023-09-22 15:09:52 -0300

    criado component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import logo from '../../../public/images/logo.svg';
+
+
+import { ActiveLink } from '../ActiveLink';
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <ActiveLink href='/' activeClassName={styles.active}>
+                    <p>
+                        <Image src={logo} alt='logo-imagem' />
+                    </p>
+                </ActiveLink>
+
+                <nav>
+                    <ActiveLink href='/' activeClassName={styles.active}>
+                        <p>Home</p>
+                    </ActiveLink>
+                    <ActiveLink href='/posts' activeClassName={styles.active}>
+                        <p>Conteúdo</p>
+                    </ActiveLink>
+                    <ActiveLink href='/sobre' activeClassName={styles.active}>
+                        <p>Quem Sou?</p>
+                    </ActiveLink>
+                </nav>
+
+                <a className={styles.gitButton} type='button' href='https://github.com/trsmrs' target='_blank'>REPOSITÓRIOS</a>
+            </div>
+        </header>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Teste</h1>
+  
     </div>
   )
 }
