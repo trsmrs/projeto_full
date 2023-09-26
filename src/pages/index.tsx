e180b6944f4db7260c0a1e4c859bd30ea9f3705c
Tiago <trsminfo@gmail.com>
2023-09-26 14:15:47 -0300

    configurando o prismic getstaticprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 import Image from 'next/image'
 
 import techsImage from '@/public/images/techs.svg'
 
-export default function Home() {
+import {getPrismicClient} from '../services/prismic'
+import Prismic from '@prismicio/client'
+import {RichText} from 'prismic-dom';
+
+
+type Content = {
+  title: string;
+  titleContent: string;
+  linkAction: string;
+  mobileTitle: string;
+  mobileContent: string;
+  mobileBanner: string;
+  webTitle: string;
+  webContent: string;
+  webBanner: string;
+}
+
+interface ContentProps{
+  content: Content
+}
+
+export default function Home({content}: ContentProps) {
+  console.log(content)
   return (
     <>
       <Head>
@@ -57,4 +80,38 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () =>{
+
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+      Prismic.Predicates.at('document.type', 'home')
+    ])
+
+   const {
+    title, sub_title, link_action,
+    mobile, mobile_content, mobile_bannser,
+    title_web,  web_content, web_banner
+   } = response.results[0].data;
+
+   const content = {
+    title: RichText.asText(title),
+    titleContent: RichText.asText(sub_title),
+    linkAction: link_action.url,
+    mobileTitle: RichText.asText(mobile),
+    mobileContent: RichText.asText(mobile_content),
+    mobileBanner: mobile_bannser.url,
+    webTitle: RichText.asText(title_web),
+    webContent: RichText.asText(web_content),
+    webBanner: web_banner.url
+   }
+
+  return{
+    props:{
+      content
+    },
+    revalidate: 60 * 2
+  }
 }
