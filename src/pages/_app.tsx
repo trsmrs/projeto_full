61ac1843cb99569086f58e4f6d3b7e1a327b5129
Tiago <trsminfo@gmail.com>
2023-10-04 16:18:01 -0300

    metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,25 +3,6 @@ import '../styles/global.scss'
 
 import { Header } from '../components/Header'
 
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Trsm',
-  description: '',
-  openGraph: {
-    images: ['https://images.prismic.io/trsm/c80277d5-a786-45a2-9c33-ea96f85c8eed__77bd0436-a0b0-4bf8-bb4b-71774c16b781-removebg.png?auto=compress,format']
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: true
-    }
-  }
-}
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
