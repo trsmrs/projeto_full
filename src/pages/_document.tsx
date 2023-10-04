9afc46056733721dbda5a591888d87930fbd923f
Tiago <trsminfo@gmail.com>
2023-10-04 16:24:22 -0300

    metadados

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,23 @@
+import { Metadata } from 'next'
 import { Html, Head, Main, NextScript } from 'next/document'
 
-
+export const metadata: Metadata = {
+  title: 'Trsm |',
+  description: '',
+  keywords: ['HTML', 'CSS', 'JAVASCRIPT', 'REACT'],
+  openGraph: {
+    images: ['https://images.prismic.io/trsm/c80277d5-a786-45a2-9c33-ea96f85c8eed__77bd0436-a0b0-4bf8-bb4b-71774c16b781-removebg.png?auto=compress,format']
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true
+    }
+  }
+}
 
 export default function Document() {
   return (
