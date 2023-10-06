09d3b510f7c669f097fa655eeef3fc13396c393b
Tiago <trsminfo@gmail.com>
2023-10-06 10:36:28 -0300

    analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Analytics from '@/src/components/Analytics/index'
+import Analytics from '../components/Analytics'
 
 export default function Document() {
   return (
