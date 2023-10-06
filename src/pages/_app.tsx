45dddfd10975a5f85c7d0b6c0a7bb91eb128cc7f
Tiago <trsminfo@gmail.com>
2023-10-06 09:27:45 -0300

    google analytic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,41 +7,34 @@ import { Header } from '../components/Header'
 import { useRouter } from 'next/router'
 import Script from 'next/script'
 import { useEffect } from 'react'
-import * as gtag from '../lib/gtag'
+import { GTM_ID, pageview } from '../lib/gtm'
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
-
   useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('routeChangeComplete', pageview)
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('routeChangeComplete', pageview)
     }
   }, [router.events])
 
+
   return (
     <>
-      <Header />
-      <script
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
         }}
       />
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
+      <Header />
       <Component {...pageProps} />
     </>
   )
