c0bf0f01fd81c2f51648bb1912fd042a4cd4480c
Tiago <trsminfo@gmail.com>
2023-10-06 11:00:59 -0300

    analytics-1

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,26 +2,7 @@ import styles from './styles.module.scss';
 import Image from 'next/image';
 import logo from '../../../public/images/logos.png';
 
-
 import { ActiveLink } from '../ActiveLink';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-    description: '',
-    keywords: ['HTML', 'CSS', 'JAVASCRIPT', 'REACT'],
-    openGraph: {
-      images: ['https://images.prismic.io/trsm/c80277d5-a786-45a2-9c33-ea96f85c8eed__77bd0436-a0b0-4bf8-bb4b-71774c16b781-removebg.png?auto=compress,format']
-    },
-    robots: {
-      index: true,
-      follow: true,
-      googleBot: {
-        index: true,
-        follow: true,
-        noimageindex: true
-      }
-    }
-  }
 
 export function Header() {
     return (
